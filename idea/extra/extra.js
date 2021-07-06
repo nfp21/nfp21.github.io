953aaf12bcba392a6cc44fbbf667cec2d7badb1d
nfp21 <82431214+nfp21@users.noreply.github.com>
2021-07-06 09:41:31 +0900

    Add files via upload

---
+++ idea/extra/extra.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+$(document).ready(function(){
+
+
+
+$(function() {
+    $('.extra').sliphover({
+        backgroundColor: 'rgba(221,17,68,.8)',
+        fontColor:'#fff'
+    });
+})
+
+$(function() {
+    $.scrollify({
+      section : ".row",
+    });
+  });
+
+
+
+});
